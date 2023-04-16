e1181bf69f0a1a2fc3d566dfe6ad20791cf93349
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-16 17:05:06 +0300

    создан app.js с express-сервером, установлен nodemon и настроен хот-релоуд

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
