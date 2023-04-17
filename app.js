27b78f4ad992b7a21fe78da3d5cd2aed04a70e75
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-17 23:08:52 +0300

    Созданы схемы и модели пользователя и карточек, созданы контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
