a5342ae4da96e60eace52625701343b63efda3ef
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-27 22:48:38 +0300

    Скрыт хеш пароля

--- models/user.js
+++ models/user.js
@@ -31,6 +31,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Поле "password" должно быть заполнено'],
       minlength: 8,
+      select: false,
     },
   },
   {
@@ -39,7 +40,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильная почта'));
