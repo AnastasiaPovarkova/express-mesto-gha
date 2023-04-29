eee315330b7d6e05c4acf5dc2fa171752e082bb4
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-29 17:27:39 +0300

    Опять валидация исправлена

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().regex(/(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/),
   }),
 }), createUser);
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-// const validator = require('validator');
 const isEmail = require('validator/lib/isEmail');
+const isURL = require('validator/lib/isURL');
 const bcrypt = require('bcryptjs');
 
 const mongoose = require('mongoose');
@@ -19,21 +19,11 @@ const userSchema = new mongoose.Schema(
       minlength: [2, 'Минимальная длина поля "name" - 2'],
       maxlength: [30, 'Максимальная длина поля "name" - 30'],
     },
-    avatar: [
-      {
-        type: String,
-        default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-        // validate: {
-        //   validator: function (value) {
-        //     const urlPattern = 1;
-        // /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/;
-        //     const urlRegExp = new RegExp(urlPattern);
-        //     return value.match(urlRegExp);
-        //   },
-        //   message: 'Введена некорректная ссылка',
-        // },
-      },
-    ],
+    avatar: {
+      type: String,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: [isURL, 'Поле "email" неверно заполнено'],
+    },
     email: {
       type: String,
       required: [true, 'Поле "email" должно быть заполнено'],
