ed3a6b6e4777efbca110d437df9215e1b05bbac6
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-29 12:43:34 +0300

    Прописаны Joi для роутов users и cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards, createCard, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    link: Joi.string(),
+  }),
+}), createCard);
 router.delete('/:cardId', deleteCardById);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers, aboutUser, getUserById, updateUserInfo, updateAvatar,
@@ -7,7 +8,16 @@ const {
 router.get('/', getUsers);
 router.get('/me', aboutUser);
 router.get('/:userId', getUserById);
-router.patch('/me', updateUserInfo);
-router.patch('/me/avatar', updateAvatar);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string(),
+  }),
+}), updateAvatar);
 
 module.exports = router;
