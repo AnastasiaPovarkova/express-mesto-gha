9420c9f41c24c0596b05cc519a66da16b152f76c
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-29 16:40:04 +0300

    Удалена валидация ссылки в модели user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const validator = require('validator');
+// const validator = require('validator');
 const isEmail = require('validator/lib/isEmail');
 const bcrypt = require('bcryptjs');
 
@@ -23,14 +23,14 @@ const userSchema = new mongoose.Schema(
       {
         type: String,
         default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-        validate: {
-          validator: function (value) {
-            const urlPattern = /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/;
-            const urlRegExp = new RegExp(urlPattern);
-            return value.match(urlRegExp);
-          },
-          message: 'Введена некорректная ссылка',
-        },
+        // validate: {
+        //   validator: function (value) {
+        //     const urlPattern = /(http|https):\/\/(\w+:{0,1}\w*#)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&amp;%#!\-/]))?/;
+        //     const urlRegExp = new RegExp(urlPattern);
+        //     return value.match(urlRegExp);
+        //   },
+        //   message: 'Введена некорректная ссылка',
+        // },
       },
     ],
     email: {
