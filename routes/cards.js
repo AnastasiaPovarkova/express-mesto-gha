d4f545094ca0ad65710cd4f358569d867c86d6a3
Anastasia Povarkova <anesthesiakorky@gmail.com>
2023-04-29 12:48:35 +0300

    Исправлены ошибки для тестов

--- routes/cards.js
+++ routes/cards.js
@@ -8,8 +8,8 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string(),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
   }),
 }), createCard);
 router.delete('/:cardId', deleteCardById);
